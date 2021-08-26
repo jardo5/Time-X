7e43ed660b2f2d5a73fb378d8707d521f0302925
Jardo <schuppjarod@gmail.com>
2021-08-26 18:47:28 -0400

    Initial Commit

---
+++ TimeX/TimeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeXApp.swift
+//  TimeX
+//
+//  Created by Jarod Schupp on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
